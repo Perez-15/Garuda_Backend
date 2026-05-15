f32891b912344fed55a5783119f42eef559fec53
Perez-15 <perezjrmh@gmail.com>
2026-05-15 23:08:25 +0800

    ClientSeeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -6,51 +6,20 @@ use Illuminate\Database\Seeder;
 use App\Models\Branch;
 use App\Models\Client;
 
-class BranchSeeder extends Seeder
+class ClientSeeder extends Seeder
 {
     public function run(): void
     {
-        $mangInasal = Client::where('name', 'Mang Inasal')->first();
-        $Burlington = Client::where('name', 'Burlington')->first();
-        $Jaicom = Client::where('name', 'Jaicom BPO Inc.')->first();
-
-        // Mang Inasal Branches
-        Branch::create([
-            'client_id' => $mangInasal->id,
-            'branch_name' => 'Mang Inasal Taguig',
-            'location' => 'Taguig City',
-            'is_active' => true,
+        Client::firstOrCreate(['name' => 'Mang Inasal'], [
+            'type' => 'Food & Beverage', 'is_active' => true,
         ]);
 
-        Branch::create([
-            'client_id' => $mangInasal->id,
-            'branch_name' => 'Mang Inasal Velasquez',
-            'location' => 'Taytay, Rizal',
-            'is_active' => true,
-        ]);
+    Client::firstOrCreate(['name' => 'Burlington'], [
+        'type' => 'Retail', 'is_active' => true,
+    ]);
 
-        Branch::create([
-            'client_id' => $mangInasal->id,
-            'branch_name' => 'Mang Inasal Ejercito',
-            'location' => 'C6, Taytay, Rizal',
-            'is_active' => true,
-        ]);
-
-        // SM Retail Branches
-        Branch::create([
-            'client_id' => $mangInasal->id,
-            'branch_name' => 'Mang Inasal Marikina Bayan',
-            'location' => 'Marikina City',
-            'is_active' => true,
-        ]);
-
-        Branch::create([
-            'client_id' => $Burlington->id,
-            'branch_name' => 'Burlington SM North',
-            'location' => 'Quezon City City',
-            'is_active' => true,
-        ]);
-
-       
-    }
+    Client::firstOrCreate(['name' => 'Jaicom BPO Inc.'], [
+        'type' => 'BPO', 'is_active' => true,
+    ]);
+}
 }
